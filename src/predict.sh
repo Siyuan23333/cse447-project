7346963caa09bedfd1ecbbc69bf86efb46d96a6d
aseinaye <119470084+aseinaye@users.noreply.github.com>
2025-02-10 19:52:14 -0800

    Update predict.sh

--- src/predict.sh
+++ src/predict.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 set -e
 set -v
-python src/myprogram.py test --work_dir work --test_data $1 --test_output $2
+# python src/myprogram.py test --work_dir work --test_data $1 --test_output $2
+
+TEST_DATA=$1
+OUTPUT_FILE=$2
+WORK_DIR="work"
+
+python myprogram.py test --work_dir "$WORK_DIR" --test_data "$TEST_DATA" --test_output "$OUTPUT_FILE"
+
